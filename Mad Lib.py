ed800923601d94a98bc31946686c2a224824832d
ElizabethO101 <33519504+ElizabethO101@users.noreply.github.com>
2017-11-09 09:30:26 -0500

    finsihed
    added ending

--- Mad Lib.py
+++ Mad Lib.py
@@ -16,7 +16,7 @@ noun1 = input()
 print("Write another noun")
 noun2 = input()
 
-print("Write another verb ending in ed")
+print("Write a verb ending in ed")
 verb2 = input()
 
 print("Write another adjective")
@@ -28,25 +28,53 @@ noun3 = input()
 print("Write another noun")
 noun4 = input()
 
+print("Write a color")
+color = input()
+
 print("Write a greeting")
 greeting1 = input()
 
 print("Write another adjective")
 adjective5 = input ()
 
+print("Write another noun")
+noun5 = input ()
+
+print("Write a conjuction")
+conjuction = input ()
+
+print("Write another verb ending in ed")
+verb3 = input()
+
+print("Write another adjective")
+adjective6 = input ()
+
+print("Write a word describing a size (big, small etc.")
+size = input ()
+
+print("Write an exclamtion")
+exclamation = input ()
+
 
 ### MAD LIB ###
 
 print("Once upon a time two  " + adjective1 + " children lived with ")
 print("their father and their wickedly " + adjective2 + " stepmother.")
-print("One day Hensel and Gretel " + verb1 + " to the woods.")
+print("One day Hansel and Gretel " + verb1 + " to the woods.")
 print("It was very " + adjective3 + " . And they were frightened.")
 print("Hensel held a " + noun1 + " in his hand to see in the dark.")
 print("They were left in the woods alone. At night they saw, (a/an) " + noun2)
-print(" coming up through the chinmney. They " + verb2 + " towards a ")
-print(adjective4 + " cottage that was made purely of " + noun3 + " It was incredible!")
+print(" coming up through a chinmney. They " + verb2 + " towards a ")
+print(adjective4 + " cottage that was made purely of " + noun3 + ". It was incredible!")
 print("Outside was the ugliest woman ever with a " + noun4)
-print(" on her skinny face. In a raspy vioce she said " + greeting1 + " 'Come in!'")
-print("My you look" + adjective5 + " let me give you something to eat.")
+print(" on her skinny face. She wore all " + color + " clothes. It was creepy.")
+print("In a raspy voice she said '" + greeting1 + " Come in!'")
+print("My you look " + adjective5 + " let me give you something to eat.")
+print("The creepy " + noun5 + " asked Gretel to get something from inside the oven")
+print("Gretel knew it was a trick " + conjuction + " she and Hansel ")
+print(verb3 + " the " + adjective6 + " witch, into the " + size + " oven.")
+print("They closed the lid shut and locked the witch in.")
+print("They heard the evil witch scream her last word, " + exclamation)
+print("Now you know the true story of Hansel and Gretel.")
 
-      
+print("Play again?")
